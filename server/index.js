146fcf522ce74c7f0c5ebe3005f14825d3ad4d6f
oweis khaliq <oweiskhaliq9@gmail.com>
2025-02-04 12:57:57 +0500

    create the add task route and vercel.json file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+import todoModel from "./models/Todos.js";
+dotenv.config();
 
 const app = express();
 
@@ -10,6 +13,18 @@ app.use(express.json());
 app.get("/", (req, res) => {
   return res.json("Server is running");
 });
+
+mongoose.connect(process.env.MONGOOSE_URI);
+
+app.post("/add", (req, res) => {
+  todoModel
+    .create({
+      task: req.body.task,
+    })
+    .then((resut) => res.json(res))
+    .catch((error) => res.json(error));
+});
+
 const PORT = 3001 || process.env.PORT;
 app.listen(PORT, () => {
   console.log("server is running on prot: http://localhost:", PORT);
--- server/models/Todos.js
+++ server/models/Todos.js
@@ -1,1 +1,10 @@
- 
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema({
+  task: {
+    type: String,
+  },
+});
+
+const todoModel = mongoose.model("task", todoSchema);
+export default todoModel;
